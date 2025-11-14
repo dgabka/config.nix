16bbcc55beae1a1cee336e4a7b840ad9a30ce378
Dawid Gąbka <dawid.gabka@grandparade.co.uk>
2025-11-14 11:58:52 +0100

    temporarily disable k9s skin

--- modules/home-manager/modules/k9s.nix
+++ modules/home-manager/modules/k9s.nix
@@ -3,11 +3,11 @@
   lib,
   ...
 }: let
-  sageveilSkinsDir = "${config.home.homeDirectory}/repos/sageveil/dist/ports/k9s";
+  # sageveilSkinsDir = "${config.home.homeDirectory}/repos/sageveil/dist/ports/k9s";
 in {
   programs.k9s = {
     enable = lib.mkDefault true;
-    settings.k9s.ui.skin = "sageveil";
-    skins.sageveil = "${sageveilSkinsDir}/sageveil.yaml";
+    # settings.k9s.ui.skin = "sageveil";
+    # skins.sageveil = "${sageveilSkinsDir}/sageveil.yaml";
   };
 }
