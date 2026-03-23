ec603ee8152c8ebe83f22d88ebdbfe9b3dc4ece3
Dawid Gąbka <gabka.daw@gmail.com>
2026-03-23 14:55:55 +0100

    feat(home-manager): add git worktree tms helpers

--- modules/home-manager/modules/scripts.nix
+++ modules/home-manager/modules/scripts.nix
@@ -3,5 +3,9 @@
     # Keep the zsh shebang from the script file.
     (pkgs.writeScriptBin "obsidian-quick-note"
       (builtins.readFile ../scripts/obsidian-quick-note.zsh))
+    (pkgs.writeShellScriptBin "git-worktree-tms"
+      (builtins.readFile ../scripts/git-worktree-tms.bash))
   ];
+
+  xdg.configFile."just/worktree.just".source = ../just/worktree.just;
 }
