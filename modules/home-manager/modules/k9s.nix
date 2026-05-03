f023a6b156549f467b3521139a278ca73afdcc03
Dawid Gąbka <gabka.daw@gmail.com>
2026-05-04 01:55:41 +0200

    feat(home-manager): enable sageveil skin for k9s

--- modules/home-manager/modules/k9s.nix
+++ modules/home-manager/modules/k9s.nix
@@ -1,13 +1,18 @@
 {
-  config,
   lib,
+  pkgs,
   ...
 }: let
-  # sageveilSkinsDir = "${config.home.homeDirectory}/repos/sageveil/dist/ports/k9s";
+  sageveil = pkgs.fetchFromGitHub {
+    owner = "sageveil";
+    repo = "k9s";
+    rev = "0.2.5";
+    sha256 = "sha256-SuBPD876dIEtPjUx61AnziHUMjmJ+r1pIf2aRK0OpZ8=";
+  };
 in {
   programs.k9s = {
     enable = lib.mkDefault true;
-    # settings.k9s.ui.skin = "sageveil";
-    # skins.sageveil = "${sageveilSkinsDir}/sageveil.yaml";
+    settings.k9s.ui.skin = "sageveil";
+    skins.sageveil = "${sageveil}/sageveil.yaml";
   };
 }
