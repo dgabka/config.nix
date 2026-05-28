1f05e120decb725c8ad03077bac3135dd4db218a
Dawid Gąbka <gabka.daw@gmail.com>
2026-05-29 00:47:54 +0200

    chore(flake): update inputs and add numtide cache

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "config.nix";
   nixConfig = {
-    extra-substituters = ["https://cache.numtide.com" "https://nix-community.cachix.org"];
-    extra-trusted-public-keys = ["niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    extra-substituters = ["https://cache.numtide.com" "https://nix-community.cachix.org" "https://numtide.cachix.org"];
+    extra-trusted-public-keys = ["niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="];
   };
   inputs = {
     # Main package source (kept on unstable to match nix-darwin master)
