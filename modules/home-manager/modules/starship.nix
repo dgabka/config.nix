a1544216b91eb1a2701c92b6e3c3e9833bb9d860
Dawid Gąbka <gabka.daw@gmail.com>
2026-07-09 00:44:36 +0200

    feat(starship): use sageveil theme

--- modules/home-manager/modules/starship.nix
+++ modules/home-manager/modules/starship.nix
@@ -1,27 +1,15 @@
-{...}: {
+{pkgs, ...}: let
+  theme = pkgs.fetchFromGitHub {
+    owner = "sageveil";
+    repo = "starship";
+    rev = "v0.2.0";
+    sha256 = "sha256-61AXJ5eZNZMok+9Aq6oGA3iywvK69nnJdM8aqvhJb1I=";
+  };
+in {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = false;
-    settings = {
-      add_newline = true;
-      aws.disabled = true;
-      command_timeout = 5000;
-      docker_context.disabled = true;
-      nix_shell = {
-        symbol = "❄️";
-        format = "via [$symbol$name]($style) ";
-        style = "blue";
-        heuristic = true;
-      };
-      git_branch.style = "purple";
-      character = {
-        success_symbol = "[❯](bold green)";
-        error_symbol = "[✗](bold red)";
-        vimcmd_symbol = "[](bold cyan)";
-        vimcmd_visual_symbol = "[](bold magenta)";
-        vimcmd_replace_symbol = "[](bold green)";
-      };
-    };
+    settings = builtins.fromTOML (builtins.readFile "${theme}/sageveil.toml");
   };
 }
