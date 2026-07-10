8e38c26fc36429a4811e0bc95167ca7a9f090fb2
Dawid Gąbka <gabka.daw@gmail.com>
2026-07-10 23:41:32 +0200

    fix(fzf): use renamed widget options

--- modules/home-manager/modules/fzf.nix
+++ modules/home-manager/modules/fzf.nix
@@ -10,15 +10,17 @@ in {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
-    changeDirWidgetCommand = "fd --type d --hidden";
-    changeDirWidgetOptions = [
-      "--height 50%"
-      "--preview 'tree -C {} | head -200'"
-      "--border=none"
-      "--preview-window 'border-none'"
-      "--no-separator"
-      "--no-scrollbar"
-    ];
+    changeDirWidget = {
+      command = "fd --type d --hidden";
+      options = [
+        "--height 50%"
+        "--preview 'tree -C {} | head -200'"
+        "--border=none"
+        "--preview-window 'border-none'"
+        "--no-separator"
+        "--no-scrollbar"
+      ];
+    };
     defaultCommand = "fd --type f --hidden";
     defaultOptions = [
       "--height 50%"
@@ -27,15 +29,17 @@ in {
       "--no-separator"
       "--no-scrollbar"
     ];
-    fileWidgetCommand = "fd --type f --hidden";
-    fileWidgetOptions = [
-      "--walker-skip .git,node_modules"
-      "--preview 'bat -n --color=always {}'"
-      "--preview-window 'border-none'"
-      "--border=none"
-      "--no-separator"
-      "--no-scrollbar"
-    ];
+    fileWidget = {
+      command = "fd --type f --hidden";
+      options = [
+        "--walker-skip .git,node_modules"
+        "--preview 'bat -n --color=always {}'"
+        "--preview-window 'border-none'"
+        "--border=none"
+        "--no-separator"
+        "--no-scrollbar"
+      ];
+    };
     tmux.enableShellIntegration = true;
     colors = import "${theme}/sageveil.nix";
     # colors = import "$HOME/repos/sageveil/dist/ports/fzf/sageveil.nix";
