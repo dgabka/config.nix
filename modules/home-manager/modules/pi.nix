2d8c6c646d3984041ed51af561e5cc0fccc3b14b
Dawid Gąbka <gabka.daw@gmail.com>
2026-07-11 03:05:24 +0200

    chore(pi): configure agent packages

--- modules/home-manager/modules/pi.nix
+++ modules/home-manager/modules/pi.nix
@@ -8,4 +8,20 @@ in {
   home.packages = [
     llmAgentPackages.pi
   ];
+
+  home.file.".pi/agent/settings.json" = {
+    force = true;
+    text = builtins.toJSON {
+      theme = "sageveil";
+      defaultProvider = "openai-codex";
+      defaultModel = "gpt-5.6-terra";
+      defaultThinkingLevel = "medium";
+      packages = [
+        "npm:@sageveil/pi@0.2.2"
+        "git:github.com/DietrichGebert/ponytail@14a0d79548d4de8fc2de95c1b94bb0de63a739d3"
+        "npm:@narumitw/pi-codex-usage@0.13.0"
+        "npm:@narumitw/pi-subagents@0.13.0"
+      ];
+    };
+  };
 }
