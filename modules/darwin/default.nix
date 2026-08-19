649fdf3f41184b0c482b30082ca4668a8e4c6435
Dawid Gąbka <gabka.daw@gmail.com>
2026-08-19 23:03:38 +0200

    feat(darwin): enable dictation

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -25,6 +25,7 @@
   system.defaults.NSGlobalDomain.KeyRepeat = 1;
   system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
   system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
+  system.defaults.CustomUserPreferences."com.apple.HIToolbox".AppleDictationAutoEnable = 1;
   system.defaults.finder.FXDefaultSearchScope = "SCcf";
   system.defaults.finder.ShowPathbar = true;
   system.defaults.finder.ShowStatusBar = true;
